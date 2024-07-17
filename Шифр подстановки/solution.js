fd03d6dfb24b87f7f219f3e43a23732adf2c49fa
grzegorzkiselev <egorkakiselev@gmail.com>
2024-07-18 02:11:17 +0300

    save

--- Шифр подстановки/solution.js
+++ Шифр подстановки/solution.js
@@ -13,51 +13,23 @@
  * @returns {string}
  */
 
-// Специальных преобразований регистра делать не нужно, заглавные и строчные буквы - это разные буквы:
-// decode('Aa', [{ from: 'a', to: 'b' }]) === 'Ab'
-
-// Части исходного сообщения, не требующие замены, остаются, как есть:
-// decode('ab', [{ from: 'a', to: 'b' }]) === 'bb'
-
-// Уже заменённые символы нельзя заменять повторно.:
-// decode('ab', [{ from: 'a', to: 'ba' }, { from: 'b', to: 'r' }]) === 'bar'
-
-// Зашифрованное сообщение расшифровывается слева направо. Замена, встретившаяся в слове раньше, имеет приоритет.:
-// decode('ab', [{ from: 'b', to: 'bar' }, { from: 'ab', to: 'foo' }]) === 'foo'
-
-
-
 function decode(message, replaces) {
-  const replacementsMap = new Map();
-  if (!message) {
-    return message;
-  }
-
-  if (!replaces || replaces.length === 0) {
-    return message;
-  }
-
-  replaces.forEach((replacement) => {
-    const key = replacement.from[0];
-    const value = replacementsMap.get(key);
-    value
-      ? value.push(replacement)
-      : replacementsMap.set(key, [replacement]);
-  })
-
   let result = "";
   for (let i = 0; i < message.length; i++) {
-    const letter = message[i];
-    const currentReplacements = replacementsMap.get(letter);
+    const firstChar = message[i];
+    let currentFrom = firstChar;
+    let currentTo = firstChar;
 
-    if (!currentReplacements || currentReplacements.length === 0) {
-      result += letter;
-      continue;
+    for (const { from, to } of replaces) {
+      if (message.substring(i, i + from.length) === from) {
+        currentFrom = from;
+        currentTo = to;
+      }
     }
 
-    const { from, to } = currentReplacements.pop();
-    i += from.length - 1;
-    result += to;
+    result += currentTo;
+    i += currentFrom.length - 1;
+
   }
 
   return result;
